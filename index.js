29978e3c764e50e23cf4c2309766fc8a79ad7ea9
PiratheeshG <piratheesh.gunaseelan@ontariotechu.net>
2025-11-07 17:16:37 -0500

    installing mroe packages (express gen)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var connect = require('connect')
 // create app
 var app = connect()
 
+
+/*
 function HomePage(req,res,next)
 {
   res.setHeader('Content-Type', 'text/plain');
@@ -13,6 +15,19 @@ app.use('/Piratheesh Gunaseelan' ,HomePage);
 app.listen(3000);
 console.log('Server is running on local host')
 
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
@@ -33,19 +48,3 @@ console.log('Server is running on local host')
 
 
 
-
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
-});
