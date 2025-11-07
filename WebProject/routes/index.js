0318d786bfa42d49e24c730d89a4f096e7a455ad
PiratheeshG <piratheesh.gunaseelan@ontariotechu.net>
2025-11-07 18:21:58 -0500

    adjusting route files (index)

--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Home' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home' });
+});
+
+/* GET about. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { 
+    title: 'About' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'About Page' });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home Page' });
 });
 
 module.exports = router;
